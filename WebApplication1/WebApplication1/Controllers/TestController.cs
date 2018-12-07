adc52a6cfad3033dd79e877cf9e21b295d6838bc
1305779886 <1305779886@qq.com>
2018-12-07 09:06:33 +0800

    123

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "你好，MVC！";
+        }
+        public Customer getCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerName = "abc";
+            ct.Address = "def";
+            return ct;
+        }
+    }
+}
